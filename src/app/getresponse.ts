d37bac357a764c5347849a93f45d371e129753b9
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-11-09 20:55:28 -0500

    update template

---
+++ src/app/getresponse.ts
@@ -0,0 +1,7 @@
+import { Product } from "./common/product";
+
+export interface Getresponse {
+  _builtIn: {
+    products: Product[];
+  };
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, map } from "rxjs";
+import { Product } from "../common/product";
+import { Getresponse } from "../getresponse";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ProductService {
+  // define a base url for the spring service
+  private baseServiceUrl = "http://localhost:8080/api/products";
+
+  constructor(private httpClient: HttpClient) {}
+
+  // method return an observable of product array
+  // map json data from spring data rest to a product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClient
+      .get<any>(this.baseServiceUrl)
+      .pipe(map((data) => data._embedded.products));
+  }
+}
